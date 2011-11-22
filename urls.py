3fa074be8630fe4d50197939e929bdc650dfee65
aayushbahuguna <aayushbahuguna@gmail.com>
2011-11-22 22:06:17 +0530

    Added api support using django rest framework

--- urls.py
+++ urls.py
@@ -1,10 +1,33 @@
 from django.conf.urls.defaults import patterns, include, url
-
+from djangorestframework.resources import ModelResource
+from djangorestframework.views import ListOrCreateModelView, InstanceModelView
+from BankCreditCard.creditcard.models import *
+from BankCreditCard.creditcard.resources import *
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+#class User(ModelResource):
+#    model = User
+    
 urlpatterns = patterns('',
+    url(r'^creditcard/api/user$',ListOrCreateModelView.as_view(resource=UserResource), name='model-resource-User'),
+    url(r'^creditcard/api/user/(?P<pk>[0-9]+)/$', InstanceModelView.as_view(resource=UserResource)),
+    url(r'^creditcard/api/personaldetail$',ListOrCreateModelView.as_view(resource=PersonalDetailResource), name='model-resource-PersonalDetail'),
+    url(r'^creditcard/api/personaldetail/(?P<pk>[0-9]+)/$', InstanceModelView.as_view(resource=PersonalDetailResource)),
+    url(r'^creditcard/api/employmentdetail$',ListOrCreateModelView.as_view(resource=EmploymentDetailResource), name='model-resource-EmploymentDetails'),
+    url(r'^creditcard/api/employmentdetail/(?P<pk>[0-9]+)/$', InstanceModelView.as_view(resource=EmploymentDetailResource)),
+    url(r'^creditcard/api/bankdetail$',ListOrCreateModelView.as_view(resource=BankDetailResource), name='model-resource-BankDetails'),
+    url(r'^creditcard/api/bankdetail/(?P<pk>[0-9]+)/$', InstanceModelView.as_view(resource=BankDetailResource)),
+    url(r'^creditcard/api/card$',ListOrCreateModelView.as_view(resource=CardResource), name='model-resource-Card'),
+    url(r'^creditcard/api/card/(?P<pk>[0-9]+)/$', InstanceModelView.as_view(resource=CardResource)),
+    url(r'^creditcard/api/statement$',ListOrCreateModelView.as_view(resource=StatementResource), name='model-resource-Statement'),
+    url(r'^creditcard/api/statement/(?P<pk>[0-9]+)/$', InstanceModelView.as_view(resource=StatementResource)),
+    url(r'^creditcard/api/autopay$',ListOrCreateModelView.as_view(resource=AutopayResource), name='model-resource-Autopay'),
+    url(r'^creditcard/api/autopay/(?P<pk>[0-9]+)/$', InstanceModelView.as_view(resource=AutopayResource)),
+    
+    
+    
     (r'^creditcard/api/payment_api.html/account_no=(?P<account_no>\d+)/amount=(?P<amount>\d+)/$', 'creditcard.views.payment_api'),
     (r'^creditcard/api/successpayment_api.html/(?P<account_no>\d+)/(?P<amount>\d+)/$', 'creditcard.views.successpayment_api'),
     (r'^creditcard/home/$', 'creditcard.views.index'),
@@ -42,4 +65,7 @@ urlpatterns += patterns('django.views.static',
 
 urlpatterns += patterns('django.views.static',
 	(r'^creditcard/user/(?P<path>.*)$', 'serve', {'document_root': 'static'}),
+)
+urlpatterns += patterns('django.views.static',
+    (r'^(?P<path>.*)$', 'serve', {'document_root': 'static'}),
 )
